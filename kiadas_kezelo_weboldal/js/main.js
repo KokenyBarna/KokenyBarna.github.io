12941c57c89db5a35b3d79cf65291cea494d2fae
KokenyBarna <85619764+KokenyBarna@users.noreply.github.com>
2021-12-05 00:28:41 +0100

    Bejelentkezes added

---
+++ kiadas_kezelo_weboldal/js/main.js
@@ -0,0 +1,31 @@
+/*=============== SHOW / HIDDEN INPUT ===============*/
+
+const showHiddenInput = (inputOverlay, inputPass, inputIcon) => {
+    const overlay = document.getElementById(inputOverlay),
+        input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+
+    iconEye.addEventListener('click', () => {
+
+        //Change password to text
+        if (input.type === 'password') {
+            // Switch to text
+            input.type = 'text'
+
+            //Change icon
+            iconEye.classList.add('bx-show')
+        } else {
+            // Change to password
+            input.type = 'password'
+                // remove icon
+            iconEye.classList.remove('bx-show')
+        }
+
+        // Toggle the overlay
+        overlay.classList.toggle('overlay-content')
+    })
+
+
+}
+
+showHiddenInput('input-overlay', 'input-password', 'input-hideEye-icon')
